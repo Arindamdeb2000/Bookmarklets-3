004a8a6ab0dae6d63565be2759d6e9a028baebc8
Andreas Brett <andreasbrett@users.noreply.github.com>
2017-06-18 14:48:46 +0200

    Create Strava_Export-Activity-as-TCX.js
    Bookmarklet Code (JavaScript) to export a Strava Activity in TCX format

---
+++ Strava_Export-Activity-as-TCX.js
@@ -0,0 +1,11 @@
+function isStravaActivity() {
+	var url = window.location.href;
+	var regexp = /http.?:\/\/www.strava\.com\/activities\/[0-9]+/gi;
+	return (url.match(regexp) !== null)
+}
+
+if (isStravaActivity()) {
+	window.location.href = window.location.href + "/export_tcx";
+} else {
+	alert("You're currently not on a Strava activity page.")
+}
